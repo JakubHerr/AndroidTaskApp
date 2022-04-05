005e41d827c9f7fe8b2f61ad595d3ba5f92818c9
herrmja1 <herrmja1@fit.cvut.cz>
2022-04-05 21:56:52 +0200

    added FAB and time picking function

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.tasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/tasks/TaskListFragment.kt
+++ app/src/main/java/com/example/tasks/TaskListFragment.kt
@@ -5,18 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.tasks.databinding.FragmentTaskListBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat.CLOCK_24H
+import java.util.*
 
 class TaskListFragment : Fragment() {
     var _binding: FragmentTaskListBinding? = null
     val binding: FragmentTaskListBinding
         get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         //Inflate the layout for this fragment
         _binding = FragmentTaskListBinding.inflate(inflater,container,false)
+
+        //pick time and show it in a toast on success
+        binding.fab.setOnClickListener {
+            pickTime()
+        }
+
         return binding.root
+    }
+
+    private fun pickTime() {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(CLOCK_24H)
+            .setHour(Calendar.getInstance().time.hours)
+            .setMinute(Calendar.getInstance().time.minutes)
+            .build()
+        timePicker.show(childFragmentManager,"test")
+        timePicker.addOnPositiveButtonClickListener {
+            Toast.makeText(context,"Time picked: ${timePicker.hour}:${timePicker.minute}",Toast.LENGTH_LONG).show()
+        }
     }
 }
