65ee2053c8efc3fb895ee89481bebb3dc93af6e1
herrmja1 <herrmja1@fit.cvut.cz>
2022-04-07 16:02:15 +0200

    implemented Task adding

--- app/src/main/java/com/example/tasks/ui/AddTaskFragment.kt
+++ app/src/main/java/com/example/tasks/ui/AddTaskFragment.kt
@@ -1,31 +1,29 @@
 package com.example.tasks.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import com.example.tasks.R
-import com.example.tasks.databinding.FragmentTasksBinding
-import com.example.tasks.adapters.TaskItemAdapter
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.tasks.data.AppDatabase
+import com.example.tasks.databinding.FragmentAddTaskBinding
 import com.example.tasks.viewmodels.TasksViewModel
 import com.example.tasks.viewmodels.TasksViewModelFactory
 
-class TasksFragment : Fragment() {
-    private var _binding: FragmentTasksBinding? = null
-    private val binding: FragmentTasksBinding
-        get() = _binding!!
+class AddTaskFragment : Fragment() {
+    private var _binding: FragmentAddTaskBinding? = null
+    private val binding: FragmentAddTaskBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        //Inflate the layout for this fragment
-        _binding = FragmentTasksBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        _binding = FragmentAddTaskBinding.inflate(inflater,container,false)
 
         //create viewmodel using its factory
         val application = requireNotNull(this.activity).application
@@ -35,17 +33,11 @@ class TasksFragment : Fragment() {
 
         //bind the viewmodel to the layout
         binding.vm = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
 
-        //set adapter for recyclerview and create an observer for updating
-        val adapter = TaskItemAdapter{}
-        binding.tasksList.adapter = adapter
-        viewModel.tasks.observe(viewLifecycleOwner, Observer { newData ->
-            adapter.submitList(newData)
-        })
-
-        binding.fab.setOnClickListener {
-            findNavController().navigate(R.id.action_tasksFragment_to_addTaskFragment)
+        //add new item and navigate back
+        binding.saveButton.setOnClickListener {
+            viewModel.addTask()
+            findNavController().navigate(R.id.action_addTaskFragment_to_tasksFragment)
         }
 
         return binding.root
--- app/src/main/java/com/example/tasks/viewmodels/TasksViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodels/TasksViewModel.kt
@@ -1,20 +1,58 @@
 package com.example.tasks.viewmodels
 
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tasks.data.Task
 import com.example.tasks.data.TaskDao
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
 
 class TasksViewModel(val dao: TaskDao) : ViewModel() {
     var newTaskName = ""
+    var date: Long? = null //Epoch milliseconds
     val tasks = dao.getAll()
+    val tz = TimeZone.currentSystemDefault()
 
     fun addTask() {
+        if(newTaskName.isBlank()) newTaskName = "Untitled"
         viewModelScope.launch {
             val task = Task()
             task.taskName = newTaskName
             dao.insert(task)
         }
     }
+
+    fun getDate(fm: FragmentManager) {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Choose date")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+        datePicker.show(fm,"datePicker")
+        datePicker.addOnPositiveButtonClickListener {
+            //Toast.makeText(ct,"Date picked: ${Instant.fromEpochMilliseconds(datePicker.selection!!)}",Toast.LENGTH_LONG).show()
+            date = datePicker.selection
+        }
+
+    }
+
+    fun getTime(fm: FragmentManager, ct: Context?) {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setHour(12)
+            .setMinute(0)
+            .build()
+        timePicker.show(fm,"timePicker")
+        timePicker.addOnPositiveButtonClickListener {
+            Toast.makeText(ct,"picked: ${timePicker.hour}:${timePicker.minute}",Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
