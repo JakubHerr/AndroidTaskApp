767f5546c2bfc7929afb4f9b0081a18059b487dc
JakubHerr <kuba2c@seznam.cz>
2022-04-22 13:10:26 +0200

    added alarm persistence through reboots

---
+++ app/src/main/java/com/example/tasks/BootReceiver.kt
@@ -0,0 +1,33 @@
+package com.example.tasks
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import java.util.*
+
+class BootReceiver : BroadcastReceiver() {
+
+    //set an alarm one minutes after receiving boot_completed for testing purposes
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d("BootReceiver","some broadcast was received")
+        if (intent.action == "android.intent.action.QUICKBOOT_POWERON" ||
+            intent.action == "android.intent.action.BOOT_COMPLETED") {
+            Log.d("BootReceiver","${intent.action} was received")
+            val intentNotification = Intent(context, Notification::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context.applicationContext,
+                1,
+                intentNotification,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+            val time = Calendar.getInstance()
+            time.add(Calendar.MINUTE,1)
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,time.timeInMillis,pendingIntent)
+            Log.d("BootReceiver","Exact alarm was set to $time")
+        }
+    }
+}
--- app/src/main/java/com/example/tasks/Notification.kt
+++ app/src/main/java/com/example/tasks/Notification.kt
@@ -1,6 +1,7 @@
 package com.example.tasks
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -8,10 +9,14 @@ import androidx.core.app.NotificationCompat
 
 class Notification : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        val appIntent = Intent(context,MainActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_IMMUTABLE)
+
         val notification = NotificationCompat.Builder(context, "channel id")
             .setSmallIcon(R.drawable.ic_baseline_library_add_check_24)
             .setContentTitle("Task app")
             .setContentText("You have 2 deadlines today")
+            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
 
